83be2e12e23d2daac71167a9423aeb28ecfcbc21
Kannan TN <kannan.t@prominance.com>
2021-12-29 12:47:14 +0530

    DB name change

--- server/config/keys_dev.js
+++ server/config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUri: "mongodb://prominanceerp:ProminanceERP2021@cluster0-shard-00-00.6ml2e.mongodb.net:27017,cluster0-shard-00-01.6ml2e.mongodb.net:27017,cluster0-shard-00-02.6ml2e.mongodb.net:27017/employeeappdb?authSource=admin&replicaSet=atlas-8u9ifq-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
+    mongoUri: "mongodb://prominanceerp:ProminanceERP2021@cluster0-shard-00-00.6ml2e.mongodb.net:27017,cluster0-shard-00-01.6ml2e.mongodb.net:27017,cluster0-shard-00-02.6ml2e.mongodb.net:27017/employeeappdbui?authSource=admin&replicaSet=atlas-8u9ifq-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
     secretOrKey: process.env.SECRET_OR_KEY
   };  
